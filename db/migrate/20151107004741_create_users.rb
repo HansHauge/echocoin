139e6150ef6ea3ec83104dfaf901f4f9b02ac029
warren vosper <straydogsw@gmail.com>
2015-11-06 18:57:15 -0600

    added user and charity models

---
+++ db/migrate/20151107004741_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :btc_address
+      t.string :btc_passwd_digest
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
