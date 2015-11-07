ce1a3ace72d51b150033f52d8268eb0d1e5b7d59
Hans Hauge <hansthered@gmail.com>
2015-11-06 20:52:17 -0800

    adds pry, begins to add style, getting pool totals from entries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
-  before_action :get_user
+  before_action :get_user, :get_prize_pools
 
   def show
   end
 
   private
+
+  def get_prize_pools
+    @bi_weekly = PrizePool.find_by_name('Bi-weekly')
+    @weekly = PrizePool.find_by_name('Weekly')
+    @daily = PrizePool.find_by_name('Daily')
+    @hourly = PrizePool.find_by_name('Hourly')
+  end
 
   def get_user
     if session[:name]
--- app/models/prize_pool.rb
+++ app/models/prize_pool.rb
@@ -1,2 +1,25 @@
 class PrizePool < ActiveRecord::Base
+
+  def total
+    last_date = Time.now
+    first_date = last_date - time_since_last_payout
+    time_range = first_date..last_date
+
+    entries = Entry.where(created_at: time_range)
+    total = 0.00
+
+    entries.each do |entry|
+      total += entry.total.to_i * 0.01
+    end
+
+    total
+  end
+
+  def my_age
+    (Time.now - created_at).to_i
+  end
+
+  def time_since_last_payout
+    my_age % payout_frequency
+  end
 end
