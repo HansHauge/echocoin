fd4d776e27cebcae5f612b69cb04a8a724a5e217
Hans Hauge <hansthered@gmail.com>
2015-11-07 12:05:00 -0800

    adds the btc user rough-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,33 @@
 class User < ActiveRecord::Base
-  after_create :generate_name
+  require 'rubygems'
+  require 'rpcjson'
 
+  after_create :generate_name
   validates :name, uniqueness: true
+
+  def get_balance
+    @bc ||= RPC::JSON::Client.new "http://bitcoinrpc:qGIOj9oli0g4YghvPxx4pbgpn1BK9lNm8W8z3pQtgWWXH@127.0.0.1:18332", 1.1
+    accounts = @bc.listreceivedbyaddress(0, true)
+    my_account = {}
+    my_balance = 0.0
+
+    accounts.each do |account|
+      if account["address"] == btc_address
+        my_account = account
+      end
+    end
+
+    if my_account.present?
+      my_balance = my_account["amount"].to_f
+    else
+      update_attributes!(btc_address: @bc.getnewaddress)
+      @bc.sendtoaddress(btc_address, 5.0)
+      @bc.generate(5)
+      my_balance = 5.0
+    end
+
+    my_balance
+  end
 
   private
 
--- regtest_example.rb
+++ regtest_example.rb
@@ -4,7 +4,9 @@ require 'rpcjson'
 # 1.1 is required because bitcoind does not speak 2.0
 #bc = RPC::JSON::Client.new 'http://username:password@127.0.0.1:8332', 1.1
 #bc = RPC::JSON::Client.new 'http://warrenv:warrenvPASS@127.0.0.1:18332', 1.1
-bc = RPC::JSON::Client.new 'http://warrenv:warrenvPASS@45.79.175.63:18444', 1.1
+# bc = RPC::JSON::Client.new 'http://hansh:hanshPASS@45.79.175.63:18444', 1.1
+# bc = RPC::JSON::Client.new 'http://username:password@127.0.0.1:8332', 1.1
+bc = RPC::JSON::Client.new "http://bitcoinrpc:qGIOj9oli0g4YghvPxx4pbgpn1BK9lNm8W8z3pQtgWWXH@127.0.0.1:18332", 1.1
 
 # JSON-RPC error objects are raised as Ruby objects of the type
 # RPC::JSON::Client::Error. The original object is available at
@@ -20,9 +22,10 @@ begin
   end
 
 #  puts "balance: #{bc.getbalance}"
-#  puts "balance: #{bc.listaccounts}"
+ # puts "wallet: #{bc.getwalletinfo}"
+ puts "bc.getaccount: #{bc.getaccount(mnzpBbvHeiH8LUPHYnjHso3zbJ5KsH6RGX)}"
 #  puts "getnewaddress: #{bc.getnewaddress}"
-  puts "received: #{bc.sendtoaddress('msUqRKMCvjEvvYPNgFutKhHRLDoAMp5MzT', 1.0)}"
+  # puts "received: #{bc.sendtoaddress('mnzpBbvHeiH8LUPHYnjHso3zbJ5KsH6RGX', 1.0)}"
 
 
   # Will raise error
